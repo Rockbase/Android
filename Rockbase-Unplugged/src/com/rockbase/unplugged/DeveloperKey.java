fdbe06b9dc6417bff9579e5eac580f4529fe99cf
Alex Belencoso <addax00@users.noreply.github.com>
2015-04-16 23:09:44 +0200

    Update DeveloperKey.java

--- Rockbase-Unplugged/src/com/rockbase/unplugged/DeveloperKey.java
+++ Rockbase-Unplugged/src/com/rockbase/unplugged/DeveloperKey.java
@@ -2,6 +2,6 @@ package com.rockbase.unplugged;
 
 public class DeveloperKey {
 
-    public static final String DEVELOPER_KEY = "AIzaSyBRgsDnw9TvnA4-NM7dCK7BFmzGrzsq3dA";
+    public static final String DEVELOPER_KEY = "PrivateDeveloperKey";
 
 }
